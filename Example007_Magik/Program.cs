5323549b6312d7889677fbe069c5b6525b44e6bb
Jacky_tut <netfinnov@mail.ru>
2022-11-30 19:28:40 +0300

    Example 007 Magik

---
+++ Example007_Magik/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Clear();
+//Console.SetCursorPosition(10, 4);
+//Console.WriteLine("+");
+
+int xa = 1;
+int ya = 1;
+int xb = 1;
+int yb = 5;
+int xc = 4;
+int yc = 2;
+
+Console.SetCursorPosition(xa, ya);
+Console.WriteLine("+");
+
+Console.SetCursorPosition(xb, yb);
+Console.WriteLine("+");
+
+Console.SetCursorPosition(xc, yc);
+Console.WriteLine("+");
+
+
